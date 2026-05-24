01f40894ae38ee52ab5685acb7ab5fc2abaad190
Pang <austinpang19@gmail.com>
2026-05-24 16:22:40 -0600

    Color contrast decided for the UI Design. Rough draft.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,24 +9,26 @@ function App() {
 
 
   return (
-    <>
-      <h1>Finding Promoters</h1>
+    <div className='h-screen w-screen bg-[#CCE0FF]'>
+      <h1 className='text-6xl font-bold text-[#000E24]'>Finding Promoters</h1>
 
-      <p> Determines if a sequence of DNA is a promoter or non-promoter.</p>
+      <p className='text-[#001433]'> Determines if a sequence of DNA is a promoter or non-promoter.</p>
 
       <div>
-        <textarea className='bg-black'></textarea>
+        <textarea className='bg-white text-[#001433] border border-[#001433] rounded-md p-2 w-96 h-48 resize-none' value={DNAtext} onChange={(e) => setDNAtext(e.target.value)}></textarea>
       </div>
       <button
       onClick={() => {
         console.log(DNAtext)
-      }}>
+      }}
+      className='bg-[#0052CC] text-[#E5F0FF] font-bold py-2 px-4 rounded'
+      >
 
         Predict
       </button>
  
-      <p>Examples: TTGACA, TAATACGACTCACTATAGGGAGA, GTTGACATTGATTATTGACTAGTTATTAATAGTAATCAATTACGGGGTCATTAGTTCATAGCCCATATATGGAGTTCCGCGTTACATAACTTACGGTAAAT</p>
-    </>
+      <p className='text-[#001433]'>Examples: TTGACA, TAATACGACTCACTATAGGGAGA, GTTGACATTGATTATTGACTAGTTATTAATAGTAATCAATTACGGGGTCATTAGTTCATAGCCCATATATGGAGTTCCGCGTTACATAACTTACGGTAAAT</p>
+    </div>
   )
 }
 
